683c6abfd9c519c7016ae0969bdd35731f929333
kayaakinci <102694902+kayaakinci@users.noreply.github.com>
2025-03-29 17:56:24 -0400

    haptics decision code for turn left and right and stairs written and not crashing, but not accurate possibly

--- working_files/stairs+pretrained_model.py
+++ working_files/stairs+pretrained_model.py
@@ -65,13 +65,31 @@ class Camera_object_detection:
         color_image = np.asanyarray(color_frame.get_data())
         return color_image, depth_frame
 
-    def object_detection(self, rgb_image):
+    def get_distance_from_point(self, depth_frame, center_x, center_y):
+        # convert depth frame to numpy array
+        depth_image = np.asanyarray(depth_frame.get_data())
+
+        # scale depth measurement points to color
+        x_scale = 1280 / 640
+        y_scale = 720 / 480
+
+        scaled_x = int((1/x_scale) * center_x)
+        scaled_y = int((1/y_scale) * center_y)
+
+        distance_to_centerpoint = depth_frame.get_distance(scaled_x, scaled_y)
+        return distance_to_centerpoint # in meters
+
+    
+    # def object_detection(self, rgb_image):
+    # Now returns list of objects detected along with list of distances to the center points of the objects
+    def object_detection(self, rgb_image, depth_frame):
         # Run both models
         stairs_results = self.stairs_model(rgb_image, conf=0.8)[0]
         coco_results = self.coco_model(rgb_image, conf=self.confidence_threshold)[0]
 
         # Process results
         detections = []
+        distances = []
         
         # First, process stairs detections (priority)
         for result in stairs_results.boxes.data.tolist():
@@ -89,6 +107,10 @@ class Camera_object_detection:
                 'confidence': confidence,
                 'bbox': (int(x1), int(y1), int(width), int(height))
             })
+
+            center_x = int(x1 + (width//2))
+            center_y = int(y1 + (height//2))
+            distances.append(((center_x, center_y), self.get_distance_from_point(depth_frame, center_x, center_y)))
         
         
         # Then process COCO model results
@@ -110,8 +132,12 @@ class Camera_object_detection:
                 'confidence': confidence,
                 'bbox': (int(x1), int(y1), int(width), int(height))
             })
+
+            center_x = int(x1 + (width//2))
+            center_y = int(y1 + (height//2))
+            distances.append(((center_x, center_y), self.get_distance_from_point(depth_frame, center_x, center_y)))
             
-        return detections
+        return detections, distances
 
     def get_depth_distances(self, depth_frame):
         # convert depth frame to numpy array
@@ -136,7 +162,8 @@ class Camera_object_detection:
 
         return final_distances
 
-        
+
+    
      # Drawing the bounding boxes on the image of the frame of the stream
     def draw_bounding_boxes(self, image, detections, distances):
         # Track object counts
@@ -183,7 +210,7 @@ class Camera_object_detection:
             )
 
         # draw distance circles
-        for (x, y), distance in distances.items():
+        for (x, y), distance in distances:
             cv2.circle(image, (x, y), 5, (0, 255, 0), -1)
             cv2.putText(image, f"{distance:.2f}m", (x - 30, y - 10),
                                 cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
@@ -222,13 +249,13 @@ class Camera_object_detection:
                     continue
 
                 # Detect objects
-                detections = self.object_detection(color_image)
-                distances = self.get_depth_distances(depth_image)
+                # detections = self.object_detection(color_image)
+                detections, distances = self.object_detection(color_image, depth_image)
+                # distances = self.get_depth_distances(depth_image)
 
                 if (detections != []):
-                    # decide_haptic_response(detections, walls)
                     prev_command = command
-                    command = "wall turn left"
+                    command = decide_haptic_response(detections, distances)
                 else:
                     prev_command = command
                     command = "none"
@@ -277,39 +304,76 @@ def send_haptic_command(command, prev_command):
     else:
         print("no haptics")
 
+def decide_object_action(nearest_hazard, second_hazard):
+    action = "none"
+
+    # Using global CENTER_REGION
+    x_nearest, y_nearest, w_nearest, h_nearest = nearest_hazard['bbox']
+    x_center_nearest = x_nearest + (x_nearest + w_nearest)//2
+    x_right_nearest = x_nearest + w_nearest
+    x_left_nearest = x_nearest
+
+    if (second_hazard == None):
+        if (x_center_nearest <= CENTER_REGION[0]):
+            return "object turn right"
+        else:
+            return "object turn left"
+    
+    x_second, y_second, w_second, h_second = second_hazard['bbox']
+    x_center_second = x_second + (x_second + w_second)//2
+    x_right_second = x_second + w_second
+    x_left_second = x_second
+    
+    if (x_center_nearest <= CENTER_REGION[0]):
+        if (x_center_second <= CENTER_REGION[0]):
+            action = "object turn right"
+        else:
+            # action = "object turn left"
+            return "stairs"
+    else:
+        if (x_center_second >= CENTER_REGION[0]):
+            action = "object turn left"
+        else:
+            action = "object turn right"
+    return action
+
+
 """ Deciding haptic response (need to get wall data and distance data for this function)
 based on the x location of nearest hazard in frame (in center third of frame)
 """
-# def decide_haptic_response(detections, walls):
-#     nearest_hazard = None
-#     second_nearest = None
-#     nearest_distance = None
-#     second_distance = None
-#     for detection in detections:
-#         x, y, w, h = detection['bbox']
-#         x_center = x + (x + w)//2
-#         x_right = x + w
-#         x_left = x
-#         curr_distance = None # fix later
-
-#         if (CENTER_REGION[0] < x_right or x_left < CENTER_REGION[1]):
-#             if (curr_distance >= nearest_distance):
-#                 second_distance = nearest_distance
-#                 nearest_distance = curr_distance
-#                 second_nearest = nearest_hazard
-#                 nearest_hazard = detection 
-#             elif (curr_distance >= second_distance):
-#                 second_distance = curr_distance
-#                 second_nearest = detection
-#         else:
-#             if (curr_distance >= second_distance):
-#                 second_distance = curr_distance
-#                 second_nearest = detection
-            
-#     # Deciding object haptic response to send
-#     if (detection["class_id"] == "stairs"):
-#         return "stairs"
-#     elif ():
+def decide_haptic_response(detections, distances):
+    nearest_hazard = None
+    second_nearest = None
+    nearest_distance = None
+    second_distance = None
+    for i in range(len(detections)):
+        detection = detections[i]
+        curr_distance = distances[i]
+    # for detection in detections:
+        x, y, w, h = detection['bbox']
+        x_center = x + (x + w)//2
+        x_right = x + w
+        x_left = x
+        
+        if (CENTER_REGION[0] < x_right or x_left < CENTER_REGION[1]):
+            if (curr_distance != 0 and (nearest_distance == None or curr_distance >= nearest_distance)):
+                second_distance = nearest_distance
+                nearest_distance = curr_distance
+                second_nearest = nearest_hazard
+                nearest_hazard = detection 
+            elif (curr_distance != 0 and (second_distance == None or curr_distance >= second_distance)):
+                second_distance = curr_distance
+                second_nearest = detection
+        else:
+            if (curr_distance != 0 and (second_distance == None or curr_distance >= second_distance)):
+                second_distance = curr_distance
+                second_nearest = detection
+    
+    # Deciding object haptic response to send
+    if (detection["class_id"] == "stairs"):
+        return "stairs"
+    else:
+        return decide_object_action(nearest_hazard, second_nearest)
 
 
 
